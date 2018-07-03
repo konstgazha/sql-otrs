9a42b53f01da178340126bd7b186ef0ffed215fb
Konstantin Gazha <konstgazha@yandex.ru>
2018-07-03 15:10:56 +0500

    Add sender, time of first line response

--- report.sql
+++ report.sql
@@ -1,4 +1,4 @@
-SET @date_threshold = '2018-04-01 00:00:00';
+SET @date_threshold = '2018-06-11 00:00:00';
 
 DROP TEMPORARY TABLE IF EXISTS main_ticket_info;
 CREATE TEMPORARY TABLE IF NOT EXISTS main_ticket_info AS
@@ -89,6 +89,53 @@ CREATE TEMPORARY TABLE IF NOT EXISTS moved AS
     GROUP BY th.ticket_id
 );
 
+#DROP TEMPORARY TABLE IF EXISTS first_line_emergence;
+#CREATE TEMPORARY TABLE IF NOT EXISTS first_line_emergence AS
+#(
+#	SELECT ticket_id tid, create_time
+#	FROM ticket_history
+#	WHERE create_time > @date_threshold
+#	AND queue_id = 5
+#	GROUP BY ticket_id
+#);
+
+#DROP TEMPORARY TABLE IF EXISTS first_move_or_lock;
+#CREATE TEMPORARY TABLE IF NOT EXISTS first_move_or_lock AS
+#(
+#	SELECT ticket_id tid, create_time
+#	FROM ticket_history
+#	WHERE create_time > @date_threshold
+#	AND history_type_id in (16, 17)
+#	GROUP BY ticket_id
+#);
+
+DROP TEMPORARY TABLE IF EXISTS first_line_first_response;
+CREATE TEMPORARY TABLE IF NOT EXISTS first_line_first_response AS
+(
+	SELECT first_line_emergence.tid tid,
+			 TIMESTAMPDIFF(SECOND, first_line_emergence.create_time, first_move_or_lock.create_time) as diff,
+			 first_line_emergence.create_time as first_line_emergence_time,
+			 first_move_or_lock.create_time as first_move_or_lock_time
+	FROM
+	(
+		SELECT min(id) as id, ticket_id tid, create_time
+		FROM ticket_history
+		WHERE create_time > @date_threshold
+		AND queue_id = 5
+		GROUP BY ticket_id
+	) first_line_emergence
+	LEFT JOIN
+	(
+		SELECT min(id) as id, ticket_id tid, create_time
+		FROM ticket_history
+		WHERE create_time > @date_threshold
+		AND history_type_id in (16, 17)
+		GROUP BY ticket_id
+	) first_move_or_lock
+	ON first_line_emergence.tid = first_move_or_lock.tid
+	WHERE first_line_emergence.id < first_move_or_lock.id
+);
+
 SELECT tn,
        mti.tid,
        tcreatetime,
@@ -101,9 +148,16 @@ SELECT tn,
        note,
        auto_close,
        closed,
-       moved_count
+       moved_count,
+       diff,
+       first_line_emergence_time,
+       first_move_or_lock_time
 FROM main_ticket_info mti
 LEFT JOIN client_request_info cri ON mti.tid = cri.tid
 LEFT JOIN pending_auto_close pac ON mti.tid = pac.tid
 LEFT JOIN closed_successful cs ON mti.tid = cs.tid
 LEFT JOIN moved ON mti.tid = moved.tid
+LEFT JOIN first_line_first_response flfr ON mti.tid = flfr.tid
+
+#LEFT JOIN first_line_emergence fle ON mti.tid = fle.tid
+#LEFT JOIN first_move_or_lock fmol ON mti.tid = fmol.tid
